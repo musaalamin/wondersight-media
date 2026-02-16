f1103d6881ea6e7c59efe182a6c39273fd99d89e
musaalamin <wizkalee@gmail.com>
2026-02-16 17:35:42 +0100

    Finalized Sanity integration, SEO, and Ad slotsnbhb sjjsabjsa

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -2,13 +2,12 @@ import { createClient } from 'next-sanity';
 import createImageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: '8dtezoms',
-  dataset: 'production',
+  // It now looks for the variables you just added to .env
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2024-01-01',
   useCdn: false,
 });
 
-// Use the correct builder function
 const builder = createImageUrlBuilder(client);
-
 export const urlFor = (source: any) => builder.image(source);
